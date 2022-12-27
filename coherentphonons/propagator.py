4f900e997e7eeb3d0cb7519bcb83f47f9ffc0482
Anubhab Haldar <ahaldar@bu.edu>
2022-12-26 20:35:33 -0500

    Pass kwargs to solve_ivp

--- coherentphonons/propagator.py
+++ coherentphonons/propagator.py
@@ -6,6 +6,6 @@ def make_propagator_1d(potential_functions: PotentialFunctions, mass: float, dam
     def propagate_1d(t, y):
         dy = np.zeros_like(y)
         dy[0] = y[1]
-        dy[1] = potential_functions.f(y[0]) * y[0] / mass #- damping * y[1]
+        dy[1] = (potential_functions.f(y[0]) - damping * y[1]) / mass
         return dy
     return propagate_1d
--- coherentphonons/solve.py
+++ coherentphonons/solve.py
@@ -2,6 +2,6 @@ from scipy.integrate import solve_ivp
 from typing import Callable
 
 
-def solve(propagator: Callable, t_span, y0):
-    sol = solve_ivp(propagator, t_span, y0, dense_output=True)
+def solve(propagator: Callable, t_span, y0, **kwargs):
+    sol = solve_ivp(propagator, t_span, y0, dense_output=True, **kwargs)
     return sol
