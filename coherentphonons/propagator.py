b3bb35ea36a81912bb49d55c047ea1bd7ddb1301
Chronum94 <ahaldar@bu.edu>
2023-01-11 21:33:13 -0500

    Fix propagator maker

--- coherentphonons/propagator.py
+++ coherentphonons/propagator.py
@@ -5,12 +5,12 @@ from typing import Callable
 
 def make_propagator_1d(*, potential_functions: PotentialFunctions, 
                        mass: float, 
-                       damping: float,
-                       forcing_function: Callable
+                       damping: float = 0.0,
+                       forcing_function: Callable = lambda t: 0.0,
                       ):
     def propagate_1d(t, y):
         dy = np.zeros_like(y)
         dy[0] = y[1]
-        dy[1] = (potential_functions.f(y[0]) * y[0] - damping * y[1] + forcing_function(t)) / mass
+        dy[1] = (potential_functions.f(y[0]) - damping * y[1] + forcing_function(t)) / mass
         return dy
-    return propagate_1d
+     return propagate_1d
