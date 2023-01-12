d98e2d65010500071a564f67f3e43a4d871f42aa
Chronum94 <ahaldar@bu.edu>
2023-01-12 15:42:14 -0500

    Add propagator and solver updates

--- coherentphonons/propagator.py
+++ coherentphonons/propagator.py
@@ -6,11 +6,11 @@ from typing import Callable
 def make_propagator_1d(*, potential_functions: PotentialFunctions, 
                        mass: float, 
                        damping: float = 0.0,
-                       forcing_function: Callable = lambda t: 0.0,
+                       forcing_function: Callable = lambda t, y: 0.0,
                       ):
     def propagate_1d(t, y):
         dy = np.zeros_like(y)
         dy[0] = y[1]
-        dy[1] = (potential_functions.f(y[0]) - damping * y[1] + forcing_function(t)) / mass
+        dy[1] = (potential_functions.f(y[0]) - damping * y[1] + forcing_function(t, y)) / mass
         return dy
      return propagate_1d
--- coherentphonons/solve.py
+++ coherentphonons/solve.py
@@ -3,7 +3,7 @@ from typing import Callable
 from coherentphonons.units import time_in_ps
 
 
-def solve(propagator: Callable, t_span, y0):
+def solve(propagator: Callable, t_span, y0, **kwargs):
     t_span = tuple(map(lambda x: x / time_in_ps))
-    sol = solve_ivp(propagator, t_span, y0, dense_output=True)
+    sol = solve_ivp(propagator, t_span, y0, **kwargs)
     return sol
